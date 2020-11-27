ee8371652fc90e989209c16d74c39130ba0b801b
asel <asik_asek@list.ru>
2020-11-27 15:47:53 +0300

    add threading

--- config.py
+++ config.py
@@ -1,10 +1,18 @@
-ora_user = 'your_user'
-ora_password = 'your_password'
-ora_ip = '127.0.0.1'
+ora_user = 'system'
+ora_password = 'WVh1bXl1qQNk'
+ora_ip = '192.168.231.55'
 ora_port = 1521
-sid = 'your_sid'
+sid = 'busdb'
 
 mongodb_conn_string = 'mongodb://mongo_rw:mongo_rw@localhost:27017/'
 mongodb_db = 'db1'
 mongodb_collection = 'test1'
-sql_query = u"SELECT 1 from dual"
+sql_query = u"SELECT ID FROM FKS_SAVEPOINTS.DOCUMENT "
+sql_get_data = u"SELECT DOCUMENT_IDENTIFIER, BODY FROM FKS_SAVEPOINTS.DOCUMENT where id = :1"
+sql_update_uid = "UPDATE FKS_SAVEPOINTS.DOCUMENT SET NOSQL_UID = :1 where id = :2"
+num_rows_fetch = 5
+
+#PAK
+#ora_password = 'yMZQgfvMzbAwq0AW'
+#ora_ip = '10.141.84.62'
+#sid = 'fksbusdb'
--- migration.py
+++ migration.py
@@ -2,17 +2,14 @@ import cx_Oracle
 from oracle_to_mongo import config
 from pymongo import MongoClient
 import gridfs
+from datetime import datetime
+from multiprocessing import Pool
 
-def export_data_from_oracle_to_mongodb(ora_user: str,
-                                       ora_password: str,
-                                       ora_ip: str,
-                                       ora_port: int,
-                                       sid: str,
-                                       mongodb_conn_string: str,
-                                       mongodb_db: str,
-                                       sql_query: str):
-    error_exception = None
-
+def create_ora_conn(ora_user: str,
+                    ora_password: str,
+                    ora_ip: str,
+                    ora_port: int,
+                    sid: str):
     ## create connections
     # create oracle connection
     try:
@@ -20,44 +17,88 @@ def export_data_from_oracle_to_mongodb(ora_user: str,
         oracle_conn = cx_Oracle.connect(ora_user, ora_password, dsn_tns)
     except:
         raise Exception("could not create a connection to Oracle database")
+    return oracle_conn
+
+
+def create_mongo_conn(mongodb_conn_string: str,
+                      mongodb_db: str):
 
     # create mongodb connection
     try:
         mongodb_client = MongoClient(mongodb_conn_string)
         mongodb_db = mongodb_client[mongodb_db]
     except:
-        # if an error ocurred while creating the connection to mongodb, oracle connection would be already created
-        # we need to destroy it
-        if oracle_conn is not None:
-            oracle_conn.close()
-        if mongodb_client is not None:
-            mongodb_client.close()
         raise Exception("could not create a connection to MongoDB server / database")
+    return mongodb_client, mongodb_db
 
-    ## exporting data
-    try:
-        cursor = oracle_conn.cursor()
-        print('executing the query in Oracle server...')
-        cursor.execute(sql_query)
-        fs = gridfs.GridFS(mongodb_db)
-        for record in cursor.fetchall():
-            a = fs.put(document_identifier=record[0], data=record[1])
-
-    except cx_Oracle.Error as error:
-        error_exception = error
-
-    finally:
-        cursor.close()
+def close_ora_conn(oracle_conn):
+    if oracle_conn is not None:
         oracle_conn.close()
+
+def close_mongo_conn(mongodb_client):
+    if mongodb_client is not None:
         mongodb_client.close()
 
-if __name__ == '__main__':
-    export_data_from_oracle_to_mongodb(config.ora_user,
-                                       config.ora_password,
-                                       config.ora_ip,
-                                       config.ora_port,
-                                       config.sid,
-                                       config.mongodb_conn_string,
-                                       config.mongodb_db,
-                                       config.sql_query)
+def migrate(id: int):
 
+    error_exception = None
+
+    try:
+        oracle_conn_cur = create_ora_conn(config.ora_user, config.ora_password, config.ora_ip, config.ora_port, config.sid)
+        mongodb_client, mongodb_db = create_mongo_conn(config.mongodb_conn_string, config.mongodb_db)
+        cursor_cur = oracle_conn_cur.cursor()
+        cursor_cur.execute(config.sql_get_data, [id])
+        document_identifier, data = cursor_cur.fetchone()
+        fs = gridfs.GridFS(mongodb_db, collection='nrgEvent', disable_md5=True)
+        _id = fs.put(filename=document_identifier, data=data.read(), metadata={"documentType": "nrgEvent"})
+        uid_update(str(_id), id, oracle_conn_cur)
+        cursor_cur.close()
+        close_ora_conn(oracle_conn_cur)
+        close_mongo_conn(mongodb_client)
+    except cx_Oracle.Error as error:
+        error_exception = error
+        print(error_exception)
+
+def uid_update(_id: str, id: int, oracle_conn_cur):
+    cursor_upd = oracle_conn_cur.cursor()
+    cursor_upd.execute(config.sql_update_uid, [_id, id])
+    oracle_conn_cur.commit()
+    cursor_upd.close()
+
+if __name__ == '__main__':
+
+    startTime = datetime.now()
+
+    oracle_conn = create_ora_conn(config.ora_user,
+                                  config.ora_password,
+                                  config.ora_ip,
+                                  config.ora_port,
+                                  config.sid)
+    cursor = oracle_conn.cursor()
+    cursor.execute(config.sql_query)
+    rows = cursor.fetchmany(config.num_rows_fetch)
+    while len(rows) > 0:
+        # fetch next rows
+        ids = [row[0] for row in rows]
+        pool = Pool(4)
+        results = pool.map(migrate, ids)
+        pool.close()
+        pool.join()
+        rows = cursor.fetchmany(config.num_rows_fetch)
+
+    cursor.close()
+    endTime = datetime.now()
+    print ("Время выполнения: ", endTime - startTime)
+
+    close_ora_conn(oracle_conn)
+
+
+'''
+#one thread working
+    cursor = oracle_conn.cursor()
+    cursor.execute(config.sql_query)
+    rows = cursor.fetchall()
+    ids = [row[0] for row in rows]
+    for id in ids:
+        migrate(id)
+'''
